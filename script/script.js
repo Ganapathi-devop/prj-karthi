7af322919fa539614119c2ebc3803a88547ac67d
ganapathi-devop <ganapathy1red1@gmail.com>
2021-10-16 15:44:00 +0530

    16-10-21

--- script/script.js
+++ script/script.js
@@ -1,5 +1,27 @@
 $(Document).ready(function(){
-
-
+    $('#nava-1').mouseover(function() {
+        $('#navlines-icon').css("left", "35.5%")
+    });
+    $('#nava-2').mouseover(function() {
+        $('#navlines-icon').css("left", "43%")
+    });
+    $('#nava-3').mouseover(function() {
+        $('#navlines-icon').css("left", "51.5%")
+    });
+    $('#nava-4').mouseover(function() {
+        $('#navlines-icon').css("left", "60.5%")
+    });
+    $('#nava-5').mouseover(function() {
+        $('#navlines-icon').css("left", "69%")
+    });
+    $('#nava-6').mouseover(function() {
+        $('#navlines-icon').css("left", "79%")
+    });
+    $('.btn-medium').mouseover(function() {
+        $('#navlines-icon').css("left", "35.5%")
+    });
+    $('.nav-links').mouseleave(function(){
+        $('#navlines-icon').css("left", "35.5%")
+    });
 
 });
